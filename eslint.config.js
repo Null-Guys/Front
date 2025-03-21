f11c8e03b09c400b6cf77ef4d44f4cd886e6fa90
Yeeun Park <ye6194@naver.com>
2025-03-21 17:37:18 +0900

    fix: 사용 중인데 사용하지 않는다고 경고가 뜨는 현상 해결

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,35 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       // 'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      "no-unused-vars": ["warn"],
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      'no-unused-vars': ['warn'],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
+    extends: ['plugin:react/recommended'], // 이거 하니까 사용 중인데 사용 중이지 않다는 경고 없어짐
   },
 ];
