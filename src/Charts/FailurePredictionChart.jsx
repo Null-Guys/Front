6c1af29acee3b3a2a46dc99bf6dad8ac67e21826
Yeeun Park <ye6194@naver.com>
2025-04-16 21:17:35 +0900

    fix: 고장 진단 차트 변경
    - 스택형 막대 그래프 -> 파이 차트

--- src/Charts/FailurePredictionChart.jsx
+++ src/Charts/FailurePredictionChart.jsx
@@ -3,26 +3,15 @@ import Chart from 'react-apexcharts';
 import ReactApexChart from 'react-apexcharts';
 
 export default function FailurePredictionChart({ chartWidth, chartHeight }) {
-  const [content, setContent] = useState({
-    series: [
-      {
-        name: '정상',
-        data: [99, 98, 97, 95, 90, 96, 94],
-      },
-      {
-        name: '고장',
-        data: [1, 2, 3, 5, 10, 4, 6],
-      },
-    ],
+  const [state, setState] = useState({
+    series: [97, 3],
     options: {
       chart: {
-        type: 'bar',
+        type: 'donut',
         fontFamily: 'Pretendard, sans-serif',
-        height: 350,
-        stacked: true,
-        stackType: '100%',
       },
-      colors: ['#1eca5d', '#EF4444'], // 색상 지정
+      labels: ['정상', '고장'],
+      colors: ['#1eca5d', '#EF4444'], // 정상, 고장 색상 지정
       title: {
         text: '연료 전지 고장 진단',
         align: 'left',
@@ -32,57 +21,29 @@ export default function FailurePredictionChart({ chartWidth, chartHeight }) {
           fontWeight: 'bold',
         },
       },
-      // 반응형 디자인
       // responsive: [
       //   {
       //     breakpoint: 480,
       //     options: {
+      //       chart: {
+      //         width: 200,
+      //       },
       //       legend: {
       //         position: 'bottom',
-      //         offsetX: -10,
-      //         offsetY: 0,
       //       },
       //     },
       //   },
       // ],
-      xaxis: {
-        categories: ['모델명1', '모델명2', '모델명3', '모델명4', '모델명5', '모델명6', '모델명7'],
-        labels: {
-          style: {
-            fontSize: '14px',
-            color: '#333333',
-          },
-        },
-      },
-      yaxis: {
-        title: {
-          text: '예측 정확도',
-          style: {
-            fontSize: '14px',
-            color: '#333333',
-          },
-        },
-      },
-      fill: {
-        opacity: 1,
-      },
-      legend: {
-        position: 'right',
-        offsetX: 0,
-        offsetY: 50,
-      },
     },
   });
 
   return (
-    <>
-      <ReactApexChart
-        options={content.options}
-        series={content.series}
-        type="bar"
-        width={chartWidth}
-        height={chartHeight}
-      />
-    </>
+    <ReactApexChart
+      type="donut"
+      options={state.options}
+      series={state.series}
+      width={chartWidth}
+      height={chartHeight}
+    />
   );
 }
--- src/Charts/LifePredictionChart.jsx
+++ src/Charts/LifePredictionChart.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
 import ReactApexChart from 'react-apexcharts';
 
-export default function LifePredictionChart({ chartWidth }) {
+export default function LifePredictionChart({ chartWidth, chartHeight }) {
   const [content, setContent] = useState({
     series: [
       {
@@ -93,75 +93,3 @@ export default function LifePredictionChart({ chartWidth }) {
     />
   );
 }
-
-// function LifePredictionChart({ chartWidth }) {
-//   const BasicColumnOptions = {
-//     chart: {
-//       type: 'bar',
-//       fontFamily: 'Pretendard, sans-serif',
-//     },
-//     title: {
-//       text: '연료 전지 수명 예측',
-//       align: 'left',
-//       style: {
-//         fontSize: '16px',
-//         color: '#333333',
-//         fontWeight: 'bold',
-//       },
-//     },
-//     plotOptions: {
-//       bar: {
-//         horizontal: false,
-//         columnWidth: '55%',
-//         borderRadius: 5,
-//         borderRadiusApplication: 'end',
-//       },
-//     },
-//     dataLabels: {
-//       enabled: false,
-//     },
-//     stroke: {
-//       show: true,
-//       width: 2,
-//       colors: ['transparent'],
-//     },
-//     xaxis: {
-//       categories: ['모델명1', '모델명2', '모델명3', '모델명4', '모델명5', '모델명6', '모델명7'],
-//       labels: {
-//         style: {
-//           fontSize: '14px',
-//           color: '#333333',
-//         },
-//       },
-//     },
-//     yaxis: {
-//       title: {
-//         text: '남은 수명(일)',
-//         style: {
-//           fontSize: '14px',
-//           color: '#333333',
-//         },
-//       },
-//     },
-//     fill: {
-//       opacity: 1,
-//     },
-//     tooltip: {
-//       // TODO: 툴팁 글자색 바꾸기
-//       y: {
-//         formatter: (val) => `$ ${val}일`,
-//       },
-//     },
-//   };
-
-//   const BasicColumnData = [
-//     {
-//       name: '남은 수명',
-//       data: [44, 55, 57, 56, 61, 20, 30],
-//     },
-//   ];
-
-//   return (
-//     <Chart options={BasicColumnOptions} series={BasicColumnData} type="bar" width={chartWidth} height={350} />
-//   );
-// }
