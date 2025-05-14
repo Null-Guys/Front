2b3deec7ebae1b13cc448b0a07d0a57694b681fa
Yeeun Park <ye6194@naver.com>
2025-05-14 18:18:52 +0900

    fix: 응답 받은 뒤 요청하도록 변경

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -47,33 +47,26 @@ export default function DashBoard() {
   }, [chartWidth]);
 
   useEffect(() => {
-    let timer;
     const loadInfo = async () => {
       try {
         const data = await fetchInfo(requestIdx);
-        console.log(data); // 응답 데이터
-
-        setInfo(data); // 전체 데이터 저장
-        setVoltages((prev) => [...prev, data.utot[1]]); // 배열에 누적
+        setInfo(data);
+        setVoltages((prev) => [...prev, data.utot[1]]);
         setStates((prev) => {
           const newStates = [...prev];
-          newStates[requestIdx] = data.isBreak; // 현재 인덱스 위치에 데이터 저장
+          newStates[requestIdx] = data.isBreak;
           return newStates;
         });
-        setRequestIdx((prev) => prev + 1); // 다음 요청 인덱스 증가
-        console.log(requestIdx, states);
+        // 응답 받자마자 바로 다음 요청 인덱스 증가
+        if (requestIdx < 29) {
+          setRequestIdx((prev) => prev + 1);
+        }
       } catch (error) {
         console.error('정보를 가져오는 데 실패했습니다:', error);
       }
     };
 
-    if (requestIdx === 0 && voltages.length === 0 && states.length === 0) {
-      loadInfo(); // 처음 한 번 바로 호출
-    } else if (voltages.length < 7) {
-      timer = setTimeout(loadInfo, 2000); // 이후엔 n초마다
-    }
-
-    return () => clearTimeout(timer);
+    if (requestIdx < 30) loadInfo();
   }, [requestIdx]);
 
   return (
